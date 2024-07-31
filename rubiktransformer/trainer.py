54a87a55ece4f39ed97721fa48f1f92323cab5e4
Adrien Bufort <adrien.bufort@orange.com>
2024-07-31 15:51:50 +0200

    init trainer function

--- rubiktransformer/trainer.py
+++ rubiktransformer/trainer.py
@@ -9,13 +9,38 @@ TODO in the trainer
 
 """
 
+import jax
+import jax.numpy as jnp
 
-def init_models_optimizers():
+import optax
+from flax import nnx
+
+from rubiktransformer.models import RubikTransformer, PolicyModel
+
+
+def init_models_optimizers(rngs, lr_1=1e-3, lr_2=1e-3):
+    """
+    Initializes the models and optimizers for the policy model and the transformer world model.
+
+    Args:
+        rngs (nnx.Rngs): The random key for initializing the models.
+        lr_1 (float, optional): The learning rate for the transformer world model. Defaults to 1e-3.
+        lr_2 (float, optional): The learning rate for the policy model. Defaults to 1e-3.
+
+    Returns:
+        tuple: A tuple containing the policy model, the transformer world model, the optimizer for the transformer world model, and the optimizer for the policy model.
+    """
     # init model for the transformer world model
     # init model for policy model
     # init optimizers
+    policy = PolicyModel(rngs=rngs)
+    transformer = RubikTransformer(rngs=rngs)
 
-    pass
+    # init optimizer
+    optimizer_worldmodel = nnx.Optimizer(transformer, optax.adam(lr_1))
+    optimizer_policy = nnx.Optimizer(policy, optax.adam(lr_2))
+
+    return policy, transformer, optimizer_worldmodel, optimizer_policy
 
 
 def init_learning():
@@ -25,6 +50,14 @@ def init_learning():
 
 
 def train():
+    # init learning
+
+    # init models and optimizers
+
+    # train
+
+    # evaluate
+
     pass
 
 
@@ -33,8 +66,4 @@ def learning_loop():
 
 
 def learning_step():
-    pass
-
-
-class Trainer:
     pass
